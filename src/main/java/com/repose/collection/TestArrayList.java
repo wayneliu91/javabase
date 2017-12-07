59cea3cf543695502c9dc9f682439df6559f23f9
wayneliushh <1162939869@qq.com>
2017-12-07 15:21:58 +0800

    新增：guava的Lists测试方法

--- src/main/java/com/repose/collection/TestArrayList.java
+++ src/main/java/com/repose/collection/TestArrayList.java
@@ -1,7 +1,15 @@
 package com.repose.collection;
 
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multiset;
+import com.google.common.collect.Sets;
+import com.google.common.primitives.Ints;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author 11629
@@ -10,6 +18,24 @@ public class TestArrayList {
 
     public static void main(String[] args) {
         testArrayList();
+        testLists();
+    }
+
+    private static void testLists() {
+        List<String> theseElements = Lists.newArrayList("alpha", "beta", "gamma");
+        List<Type> exactly100 = Lists.newArrayListWithCapacity(100);
+        List<Type> approx100 = Lists.newArrayListWithExpectedSize(100);
+        Set<Type> approx100Set = Sets.newHashSetWithExpectedSize(100);
+        Multiset<String> multiset = HashMultiset.create();
+
+        List countUp = Ints.asList(1, 2, 3, 4, 5);
+        // {5, 4, 3, 2, 1}
+        List countDown = Lists.reverse(countUp);
+        //{{1,2}, {3,4}, {5}}
+        List partition = Lists.partition(countUp, 2);
+        for (int i = 0; i < 3; i++) {
+            Object o = partition.get(i);
+        }
     }
 
     private static void testArrayList() {
