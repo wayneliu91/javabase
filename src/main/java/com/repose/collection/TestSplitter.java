8d36b71323d459a6854a2feb928ee1f5fb4a5179
wayneliushh <1162939869@qq.com>
2017-12-11 08:38:03 +0800

    splitter测试类增加示例代码

--- src/main/java/com/repose/collection/TestSplitter.java
+++ src/main/java/com/repose/collection/TestSplitter.java
@@ -1,8 +1,16 @@
 package com.repose.collection;
 
+import com.google.common.base.Splitter;
+
 /**
  * @author 11629
  */
 public class TestSplitter {
+
+    public static void main(String[] args) {
+        Splitter.on(' ').split("1 2 3");
+
+        Splitter.onPattern("\\s+").split("1 \t   2 3");
+    }
 
 }
