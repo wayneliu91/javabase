41953741d2a4188ab2952575b0011cf12ae26d0e
wayneliushh <1162939869@qq.com>
2017-12-28 18:05:48 +0800

    优化测试方法

--- src/test/java/com/google/common/base/PreconditionsTest.java
+++ src/test/java/com/google/common/base/PreconditionsTest.java
@@ -1,5 +1,6 @@
 package com.google.common.base;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,7 +10,7 @@ public class PreconditionsTest {
 
 
     @Test
-    public void Preconditions() throws Exception {
+    public void Preconditions() {
         getPersonByPrecondition(8, "peida");
 
         try {
@@ -30,7 +31,7 @@ public class PreconditionsTest {
             System.out.println(e.getMessage());
         }
 
-        List<Integer> intList = new ArrayList<Integer>();
+        List<Integer> intList = Lists.newArrayList();
         for (int i = 0; i < 10; i++) {
             try {
                 checkState(intList, 9);
@@ -38,7 +39,6 @@ public class PreconditionsTest {
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
-
         }
 
         try {
@@ -84,28 +84,27 @@ public class PreconditionsTest {
         }
     }
 
-    public static void getPersonByPrecondition(int age, String neme) throws Exception {
-        Preconditions.checkNotNull(neme, "neme为null");
-        Preconditions.checkArgument(neme.length() > 0, "neme为\'\'");
+    private static void getPersonByPrecondition(int age, String name) {
+        Preconditions.checkNotNull(name, "name为null");
+        Preconditions.checkArgument(name.length() > 0, "name为\'\'");
         Preconditions.checkArgument(age > 0, "age 必须大于0");
-        System.out.println("a person age:" + age + ",neme:" + neme);
-
+        System.out.println("a person age:" + age + ",name:" + name);
     }
 
-    public static void checkState(List<Integer> intList, int index) throws Exception {
+    private static void checkState(List<Integer> intList, int index) {
         //表达式为true不抛异常
         Preconditions.checkState(intList.size() < index, " intList size 不能大于" + index);
     }
 
-    public static void checkPositionIndex(List<Integer> intList, int index) throws Exception {
+    private static void checkPositionIndex(List<Integer> intList, int index) {
         Preconditions.checkPositionIndex(index, intList.size(), "index " + index + " 不在 list中， List size为：" + intList.size());
     }
 
-    public static void checkPositionIndexes(List<Integer> intList, int start, int end) throws Exception {
+    private static void checkPositionIndexes(List<Integer> intList, int start, int end) {
         Preconditions.checkPositionIndexes(start, end, intList.size());
     }
 
-    public static void checkElementIndex(List<Integer> intList, int index) throws Exception {
+    private static void checkElementIndex(List<Integer> intList, int index) {
         Preconditions.checkElementIndex(index, intList.size(), "index 为 " + index + " 不在 list中， List size为： " + intList.size());
     }
 }
